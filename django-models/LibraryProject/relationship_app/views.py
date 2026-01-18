97474a878dc60b360e6dd8df76510b95dc8eac54
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2026-01-18 20:21:42 +0100

    relationship app - user auth

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -42,7 +42,7 @@ def logout_view(request):
     return render(request, "relationship_app/logout.html")
 
 
-def register_view(request):
+def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
