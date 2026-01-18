13e8be901da86e7506a81b5a0999d90e81796529
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2026-01-18 21:58:06 +0100

    relationship app - custom permission implementation

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,18 +11,11 @@ urlpatterns = [
 
     path("register/", views.register, name="register"),
 
-     path("admin-view/", views.admin_view, name="admin_view"),
+    path("admin-view/", views.admin_view, name="admin_view"),
     path("librarian-view/", views.librarian_view, name="librarian_view"),
     path("member-view/", views.member_view, name="member_view"),
-     path("books/", views.list_books, name="list_books"),
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-    path("add-book/", views.add_book, name="add_book"),
-    path("edit-book/<int:pk>/", views.edit_book, name="edit_book"),
-    path("delete-book/<int:pk>/", views.delete_book, name="delete_book"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("edit_book/<int:pk>/", views.edit_book, name="edit_book"),
+    path("delete_book/<int:pk>/", views.delete_book, name="delete_book"),
 ]
-
-
-
-
-
