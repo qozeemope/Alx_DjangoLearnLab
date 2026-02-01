432f32f425ee2e2b59043ec4649158414d541f61
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2026-02-01 16:44:24 +0100

    crud operations

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,8 @@
-from rest_framework import generics
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class BookViewSet(ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
