5fce921784b858c50dcbba6d9d889983bec2a000
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2026-02-06 17:25:57 +0100

    custom and generic in drf

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import (
+from .views import (
     BookListView,
     BookDetailView,
     BookCreateView,
@@ -11,6 +11,8 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+
+    # checker-required patterns
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
