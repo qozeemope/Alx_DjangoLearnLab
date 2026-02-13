04394ea7790ec9cf4a2466f8d029817ff3081dc0
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2026-02-13 11:18:32 +0100

    django blog set up

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
     }
 }
+
 
 
 # Password validation
